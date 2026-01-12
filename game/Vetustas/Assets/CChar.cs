8380f9d2398ad1b80b48df916953121e1696c724
sunell <hs0647@gmail.com>
2026-01-12 16:31:12 +0900

    style. delete

--- game/Vetustas/Assets/CChar.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CChar : MonoBehaviour {
-
-    public GameObject[] mpCharObject = new GameObject[4];
-}
